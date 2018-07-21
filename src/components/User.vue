d1df98ff76b9a3f52f8d540c6206d2538f47ba06
fergv <jfer.garciav@gmail.com>
2018-07-21 01:29:42 -0500

    Create components and install libraries

---
+++ src/components/User.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div.card
+    div.card-image
+      figure.image.is-128x128
+        img(:src="user.picture.large" :alt="fullName")
+
+    div.card-content
+      div.media
+        div.media-left
+          figure.image.is-48x48
+            img(:src="user.picture.thumbnail" :alt="fullName")
+
+        div.media-content
+          p.title.is-4(v-text="fullName")
+          p.subtitle.is-6(v-text="user.email")
+
+      div.content
+        p #[b Calle]: #[span(v-text="user.location.street")]
+        p #[b Ciudad]: #[span(v-text="user.location.city")]
+        p #[b Estado]: #[span(v-text="user.location.state")]
+        p #[b Código Postal]: #[span(v-text="user.location.postcode")]
+</template>
+
+<script>
+export default {
+  props: ['user'],
+  computed: {
+    fullName() {
+      return `${this.user.name.title} ${this.user.name.first} ${this.user.name.last}`
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div
+    h1.title Listado de usuarios
+    User(v-for="user in users" :user="user")
+</template>
+
+<script>
+import User from './User.vue'
+import axios from 'axios'
+
+export default {
+  components: { User },
+  mounted() {
+    axios.get('https://randomuser.me/api/?results=20')
+          .then(res => this.users = res.data.results)
+          .catch(err => alert('No pudimos obtener los usuarios :('))
+  },
+  data() {
+    return {
+      users: []
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
